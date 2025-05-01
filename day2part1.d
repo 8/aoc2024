a2bda5226868de90890dcc05e4a3146dc44f0d23
mdk <mdk@lostindetails.com>
2025-05-01 08:31:43 +0200

    adds day2part1.d

---
+++ day2part1.d
@@ -0,0 +1,39 @@
+#!/usr/bin/rdmd
+import std;
+import std.range;
+import std.stdio;
+
+bool isSafe(int[] numbers) {
+  enum Direction { none, asc, desc }
+  auto direction = Direction.none;
+
+  foreach(n; numbers.slide(2)) {
+    auto diff = n[0]-n[1];
+    auto dir = diff < 0 ? Direction.asc : Direction.desc;
+
+    if (direction == Direction.none)
+      direction = dir;
+    else if (direction != dir)
+      return false;
+    
+    auto a = abs(diff);
+
+    if (!(a >=1 && a <= 3))
+      return false;
+  }
+  return true;
+}
+
+void main() {
+  auto file = File("02.txt", "r");
+
+  int safe = 0;
+  string line;
+  while ((line = file.readln()) !is null) {
+    int[] numbers = split(line).map!(s => to!int(s)).array;
+    if (isSafe(numbers))
+      safe++;
+  }
+
+  writefln("result: %s", safe);
+}
